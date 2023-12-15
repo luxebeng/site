3203d2c0a5ebd0f28233fbb7de0c262cd8020f64
luxebeng <luxebeng@gmail.com>
2023-12-15 08:38:44 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
 const path = require("path");
 module.exports = {
-  entry: "./index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index.js",
+  module: {
+    rules: [
+      {
+        test: /\.wasm$/,
+        type: 'webassembly/sync',
+      }
+    ]
   },
+  experiments: {
+    syncWebAssembly: true
+  },
+  entry: "./index.js",
   mode: "development",
 };
